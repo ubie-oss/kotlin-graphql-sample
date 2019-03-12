81cf1e2217db700c5683dd706bbb58339aaa3678
Toshihiro Yagi <sylc.yagi@gmail.com>
2019-03-12 15:24:16 +0900

    Fix typo.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,13 +3,13 @@ rootProject.name = "kotlin-graphql-sample"
 pluginManagement {
     resolutionStrategy {
         val kotlinVersion: String by settings
-        val sprintBootVersion: String by settings
+        val springBootVersion: String by settings
         eachPlugin {
             when (requested.id.id) {
                 "org.jetbrains.kotlin.jvm" -> useModule("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
                 "org.jetbrains.kotlin.plugin.spring" -> useModule("org.jetbrains.kotlin:kotlin-allopen:$kotlinVersion")
                 "org.jetbrains.kotlin.plugin.jpa" -> useModule("org.jetbrains.kotlin:kotlin-noarg:$kotlinVersion")
-                "io.spring.dependency-management", "org.springframework.boot" -> useModule("org.springframework.boot:spring-boot-gradle-plugin:$sprintBootVersion")
+                "io.spring.dependency-management", "org.springframework.boot" -> useModule("org.springframework.boot:spring-boot-gradle-plugin:$springBootVersion")
                 "org.jlleitschuh.gradle.ktlint" -> useModule("gradle.plugin.org.jlleitschuh.gradle:ktlint-gradle:4.1.0")
                 "org.flywaydb.flyway" -> useModule("gradle.plugin.com.boxfuse.client:gradle-plugin-publishing:5.2.1")
             }
