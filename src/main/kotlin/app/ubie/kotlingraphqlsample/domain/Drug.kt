6e97c516f94f47ad23d985e4a5000ee1290e542e
shiraji <isogai.shiraji@gmail.com>
2019-07-22 01:29:10 +0900

    Add id, description and id search

--- src/main/kotlin/app/ubie/kotlingraphqlsample/domain/Drug.kt
+++ src/main/kotlin/app/ubie/kotlingraphqlsample/domain/Drug.kt
@@ -1,6 +1,8 @@
 package app.ubie.kotlingraphqlsample.domain
 
 data class Drug(
+    val id: String,
     val name: String,
+    val description: String?,
     val yjCode: String
 )
--- src/main/kotlin/app/ubie/kotlingraphqlsample/infrastructure/jdbc/JdbcDrugRepository.kt
+++ src/main/kotlin/app/ubie/kotlingraphqlsample/infrastructure/jdbc/JdbcDrugRepository.kt
@@ -5,13 +5,16 @@ import app.ubie.kotlingraphqlsample.domain.DrugRepository
 import org.springframework.jdbc.core.RowMapper
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate
 import org.springframework.stereotype.Repository
+import java.util.UUID
 
 @Repository
 class JdbcDrugRepository(private val jdbcTemplate: NamedParameterJdbcTemplate) : DrugRepository {
 
     private val rowMapper: RowMapper<Drug> = RowMapper { rs, _ ->
         Drug(
+                id = rs.getString("id"),
                 name = rs.getString("name"),
+                description = rs.getString("description"),
                 yjCode = rs.getString("yj_code")
         )
     }
@@ -22,7 +25,9 @@ class JdbcDrugRepository(private val jdbcTemplate: NamedParameterJdbcTemplate) :
         return jdbcTemplate.query(
                 """
                 SELECT
+                  id,
                   name,
+                  description,
                   yj_code
                 FROM
                   drug
@@ -39,7 +44,9 @@ class JdbcDrugRepository(private val jdbcTemplate: NamedParameterJdbcTemplate) :
         return jdbcTemplate.query(
                 """
                 SELECT
+                  id,
                   name,
+                  description,
                   yj_code
                 FROM
                   drug
@@ -47,5 +54,23 @@ class JdbcDrugRepository(private val jdbcTemplate: NamedParameterJdbcTemplate) :
                   yj_code IN (:yj_codes)
                 """.trimIndent(), mapOf("yj_codes" to yjCodes), rowMapper
         )
+    }
+
+    override fun getDrug(id: String): Drug? {
+        if (id.isEmpty()) return null
+        //language=SQL
+        return jdbcTemplate.query(
+            """
+                SELECT
+                  id,
+                  name,
+                  description,
+                  yj_code
+                FROM
+                  drug
+                WHERE
+                  id = :id
+                """.trimIndent(), mapOf("id" to UUID.fromString(id)), rowMapper
+        ).firstOrNull()
     }
 }
